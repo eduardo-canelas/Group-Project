faca7a7f512becb950f7bad158068a687b66bc9b
eduardo-canelas <ed107106@ucf.edu>
2026-03-03 12:33:35 -0500

    installed project dependencies.

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("API Running");
+});
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((error) => {
+        console.error("Failed to connect to MongoDB:", error);
+    });
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
